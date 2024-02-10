6d8eff76459a9d1b2a75b684ee9ea6110e6ce661
Herbert M. <hmorales5@tutanota.com>
2024-02-10 11:49:36 -0600

    change colors on hover

--- components/PillBtn.tsx
+++ components/PillBtn.tsx
@@ -1,9 +1,21 @@
 import { Pressable, Text, StyleSheet } from "react-native";
+import {useState} from 'react';
 
 const PillBtn = ({buttonText}) => {
+    const [pillvisual, setPillVisual] = useState(styles.PillButton);
+    const [pillTextvisual, setPillTextVisual] = useState(styles.BtnText);
     return (
-        <Pressable style={styles.PillButton}>
-            <Text style={styles.BtnText}>{buttonText}</Text>
+        <Pressable style={pillvisual}
+                   onHoverIn={() => {
+                       setPillVisual(styles.PillBtnHover);
+                       setPillTextVisual(styles.BtnTextHover);
+                   }}
+            onHoverOut={() => {
+                setPillVisual(styles.PillButton);
+                setPillTextVisual(styles.BtnText);
+            }}
+        >
+            <Text style={pillTextvisual}>{buttonText}</Text>
 
         </Pressable>
     );
@@ -24,6 +36,21 @@ const styles = StyleSheet.create({
     BtnText: {
         fontSize: 20,
         fontWeight: 'normal',
+
+    },
+    BtnTextHover:{
+        fontSize: 20,
+        fontWeight: 'normal',
+        color: "#fff",
+
+    },
+    PillBtnHover: {
+        backgroundColor: '#000',
+        borderColor: '#000',
+        borderWidth: 1,
+        borderRadius: 30,
+        paddingHorizontal: 20,
+        paddingVertical: 5,
 
     },
 })
