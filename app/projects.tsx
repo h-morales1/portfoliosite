c701bc0477f3b9d4c008d2e3542de0bb919c1dcb
Herbert M. <hmorales5@tutanota.com>
2024-02-12 14:49:13 -0600

    added projects route and projectsList component

--- app/projects.tsx
+++ app/projects.tsx
@@ -1,10 +1,17 @@
 import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import ProjectsList from '../assets/projectsList/ProjectListSvg';
 
 export default function Page() {
     return (
         <SafeAreaView>
             <View>
                 <Text>Projects</Text>
+            </View>
+            <View>
+                <ProjectsList
+                    height={"100%"}
+                    width={"100%"}
+                />
             </View>
         </SafeAreaView>
 
